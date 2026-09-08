03b52a9048cb92ec95dbfd86df846ddc80a90411
FELIPE SANTOS IENSEN <felipeiensen@alunos.utfpr.edu.br>
2026-09-08 09:17:57 -0300

    Create torneioOrdenacao.ts

---
+++ lista2exercicios/torneioOrdenacao.ts
@@ -0,0 +1,19 @@
+import ordenacao from "readline-sync";
+
+let competidor1: number = 0,
+    competidor2: number = 0,
+    competidor3: number = 0,
+    competidor4: number = 0,
+    competidor5: number = 0,
+    ordemDecrescente: number = 0;
+
+console.log("Digite a pontuação dos cinco finalistas do torneio de videogame: ");
+competidor1 = ordenacao.questionInt("Digite a pontuação do primeiro competidor: ");
+competidor2 = ordenacao.questionInt("Digite a pontuação do segundo competidor: ");
+competidor3 = ordenacao.questionInt("Digite a pontuação do terceiro competidor: ");
+competidor4 = ordenacao.questionInt("Digite a pontuação do quarto competidor: ");
+competidor5 = ordenacao.questionInt("Digite a pontuação do quinto competidor: ");
+
+ordemDecrescente = Math.max(competidor1, competidor2, competidor3, competidor4, competidor5);
+
+console.log(ordemDecrescente);
